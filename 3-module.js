2006fb2101c4a6fc7dfc37fd9bad14a48ba18914
Joe Chen <joec02730@gmail.com>
2023-03-04 11:35:57 -0500

    first commit

---
+++ 3-module.js
@@ -0,0 +1,9 @@
+
+const names  = require('./4-names')
+const sayHi  = require('./5-utils')
+const data = require('./6-alternative-flavors')
+require('./7-mind-grenade')
+// console.log(data)
+// sayHi('meow')
+// sayHi(names.weom)
+// sayHi(names.woof)
---
+++ 4-names.js
@@ -0,0 +1,6 @@
+const secret = 'super secret'
+const weom  = 'weom'
+const woof  = 'woof'
+
+module.exports = {weom, woof}
+
